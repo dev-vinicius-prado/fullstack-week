37b47921c94c1541f5d8320a51056a8b74b81d6b
dev-vinicius-prado <developer.vinicius.prado@gmail.com>
2021-06-03 13:50:43 -0300

    test(InvestimentoService): interface e implementação
    - criando testes para as operações de CRUD do investimento.

---
+++ backend/src/test/java/com/fullstackweek/myinvest/services/InvestimentosServiceTest.java
@@ -0,0 +1,74 @@
+package com.fullstackweek.myinvest.services;
+
+
+import com.fullstackweek.myinvest.domain.Investimento;
+import com.fullstackweek.myinvest.repositories.InvestimentoRepository;
+import com.fullstackweek.myinvest.services.impl.InvestimentoServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class InvestimentosServiceTest {
+
+    @Mock
+    private InvestimentoRepository investimentoRepository;
+    private InvestimentoService underTest;
+
+    @Test
+    void deve_chamar_o_buscar_por_codigo_do_ativo() {
+        //dado que
+        final var codigoDoAtivo = "AAA000";
+        //quando
+        underTest.buscarPorCodigoDoAtivo(codigoDoAtivo);
+        //entao
+        verify(investimentoRepository).findByCodigoDoAtivo(codigoDoAtivo);
+    }
+
+    @Test
+    void deve_chamar_o_buscar_por_id() {
+        //dado que
+        final var idInvestimento = 1L;
+        //quando
+        underTest.buscarPorId(idInvestimento);
+        //entao
+        verify(investimentoRepository).findById(idInvestimento);
+    }
+
+    @Test
+    void deve_chamar_o_carregar_lista_de_investimentos() {
+        //quando
+        underTest.carregarListaInvestimentos();
+        //entao
+        verify(investimentoRepository).findAll();
+    }
+
+    @Test
+    void deve_chamar_o_deletar_investimento() {
+        //dado que
+        final var investimento = new Investimento();
+        //quando
+        underTest.deletarInvestimento(investimento);
+        //entao
+        verify(investimentoRepository).delete(investimento);
+    }
+
+    @Test
+    void deve_chamar_o_registrar_investimento() {
+        //dado que
+        final var investimento = new Investimento();
+        //quando
+        underTest.registrarInvestimento(investimento);
+        //entao
+        verify(investimentoRepository).save(investimento);
+    }
+
+    @BeforeEach
+    void setUp() {
+        underTest = new InvestimentoServiceImpl(investimentoRepository);
+    }
+}
